07cf4596e1b67608f6f1fbc48f0e56b82bdbd74e
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-09-13 21:03:15 +0530

    Add files via upload

---
+++ ConsoleColor.java
@@ -0,0 +1,34 @@
+ class ConsoleColors {
+    // Reset
+    public static final String RESET = "\u001B[0m";
+
+    // Regular Colors
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+    
+    // Bold
+    public static final String BLACK_BOLD = "\u001B[30;1m";
+    public static final String RED_BOLD = "\u001B[31;1m";
+    public static final String GREEN_BOLD = "\u001B[32;1m";
+    public static final String YELLOW_BOLD = "\u001B[33;1m";
+    public static final String BLUE_BOLD = "\u001B[34;1m";
+    public static final String PURPLE_BOLD = "\u001B[35;1m";
+    public static final String CYAN_BOLD = "\u001B[36;1m";
+    public static final String WHITE_BOLD = "\u001B[37;1m";
+
+    // Background Colors
+    public static final String BLACK_BACKGROUND = "\u001B[40m";
+    public static final String RED_BACKGROUND = "\u001B[41m";
+    public static final String GREEN_BACKGROUND = "\u001B[42m";
+    public static final String YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String BLUE_BACKGROUND = "\u001B[44m";
+    public static final String PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String CYAN_BACKGROUND = "\u001B[46m";
+    public static final String WHITE_BACKGROUND = "\u001B[47m";
+}
